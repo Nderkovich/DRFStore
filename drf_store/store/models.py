58fd239678d9d20e58e249b449743ba128c1f8c7
Nderkovich <nikitaderkovich@gmail.com>
2020-11-04 14:28:57 +0300

    added models

--- drf_store/store/models.py
+++ drf_store/store/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class Role(models.Model):
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=10)
+
+
+class User(models.Model):
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    date_of_birth = models.DateField(null=False)
+
+    roles = models.ManyToManyField(Role)
+
+
+class Product(models.Model):
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+
+
+class Order(models.Model):
+    STATUSES = (
+        ('IN_PROGRESS', 'IN_PROGRESS'),
+        ('FINISHED', 'FINISHED'),
+        ('APPROVED', 'APPROVED'),
+        ('DECLINED', 'DECLINED')
+    )
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    status = models.CharField(max_length=15, choices=STATUSES, default='IN_PROGRESS')
+
+    products = models.ManyToManyField(Product)
