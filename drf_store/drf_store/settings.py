ef61a6732790621321a24c62c3a811d18c33ab03
Nderkovich <nikitaderkovich@gmail.com>
2020-11-10 14:10:37 +0300

    Added pagination

--- drf_store/drf_store/settings.py
+++ drf_store/drf_store/settings.py
@@ -127,3 +127,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Rest framework settings
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10
+}
